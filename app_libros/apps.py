68b71392596b37eda18fffd51c830d1c633e2fd0
Musarna <131316534+Musarna@users.noreply.github.com>
2025-11-03 15:32:46 -0300

    Update apps.py
    cambio en apps.py

--- app_libros/apps.py
+++ app_libros/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AppLibrosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_libros'
+
+    def ready(self):
+        from . import signals  # noqa
